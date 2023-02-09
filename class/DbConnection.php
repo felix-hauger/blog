58810438371141e11d3bc0aa21f2bb03c77af68c
felix-hauger <felix.hauger@laplateforme.io>
2023-02-09 15:27:32 +0100

    added DbConnection class child of PDO

---
+++ class/DbConnection.php
@@ -0,0 +1,25 @@
+<?php
+
+class DbConnection extends PDO
+{
+
+    public function __construct()
+    {
+        $db = parse_ini_file('../config/database.ini');
+        $type = $db['type'];
+        $name = $db['name'];
+        $host = $db['host'];
+        $user = $db['user'];
+        $password = $db['password'];
+
+        try {
+            parent::__construct($type . ':dbname=' . $name . ';host=' . $host . ';charset=utf8mb4', $user, $password);
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+}
+
+$test = new DbConnection();
+var_dump($test);
